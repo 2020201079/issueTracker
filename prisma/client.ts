ba67f2389a82e305d8613db69ea539e28bf2437e
Varun Nambigari <nambigarivarun@gmail.com>
2024-12-06 00:14:02 +0530

    api fro creating issues

---
+++ prisma/client.ts
@@ -0,0 +1,23 @@
+import { PrismaClient } from "@prisma/client";
+
+declare global {
+  namespace NodeJS {
+    interface Global {
+      prisma: any;
+    }
+  }
+}
+
+const prismaClientSingleton = () => {
+  return new PrismaClient();
+};
+
+declare const globalThis: {
+  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
+} & typeof global;
+
+const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
+
+export default prisma;
+
+if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
