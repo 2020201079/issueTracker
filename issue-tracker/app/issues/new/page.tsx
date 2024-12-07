fd2253e4804c8891a33e98859bb6ca141ecbf0a9
Varun Nambigari <nambigarivarun@gmail.com>
2024-12-07 13:31:21 +0530

    handle errors

--- issue-tracker/app/issues/new/page.tsx
+++ issue-tracker/app/issues/new/page.tsx
@@ -1,7 +1,7 @@
 "use client";
-import { TextField, TextArea, Button } from "@radix-ui/themes";
+import { TextField, TextArea, Button, Callout } from "@radix-ui/themes";
 import { useForm, Controller } from "react-hook-form";
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import "easymde/dist/easymde.min.css";
 import { useRouter } from "next/navigation";
@@ -14,23 +14,36 @@ interface IssueForm {
 const NewIssuePage = () => {
   const router = useRouter();
   const { register, control, handleSubmit } = useForm<IssueForm>();
+  const [error, setError] = useState("");
 
   return (
-    <form
-      className="max-w-xl space-y-3"
-      onSubmit={handleSubmit(async (data) => {
-        await axios.post("/api/issues", data);
-        router.push("/issues");
-      })}
-    >
-      <TextField.Root placeholder="Title" {...register("title")} />
-      <TextArea
-        placeholder="Description"
-        {...register("description")}
-      ></TextArea>
+    <div className="max-w-xl">
+      {error && (
+        <Callout.Root color="red" className="mb-5">
+          <Callout.Text>{error}</Callout.Text>
+        </Callout.Root>
+      )}
+      <form
+        className=" space-y-3"
+        onSubmit={handleSubmit(async (data) => {
+          try {
+            await axios.post("/api/issues", data);
+            router.push("/issues");
+          } catch (error) {
+            setError("An unexpected error occured.");
+            console.log(error);
+          }
+        })}
+      >
+        <TextField.Root placeholder="Title" {...register("title")} />
+        <TextArea
+          placeholder="Description"
+          {...register("description")}
+        ></TextArea>
 
-      <Button>Submit New Issue</Button>
-    </form>
+        <Button>Submit New Issue</Button>
+      </form>
+    </div>
   );
 };
 
