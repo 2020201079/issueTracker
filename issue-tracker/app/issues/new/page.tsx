40276cca625c2f5c56e14a0e4d1fdf567965d1ce
Varun Nambigari <nambigarivarun@gmail.com>
2024-12-08 11:17:47 +0530

    error handling at client side

--- issue-tracker/app/issues/new/page.tsx
+++ issue-tracker/app/issues/new/page.tsx
@@ -1,19 +1,30 @@
 "use client";
-import { TextField, TextArea, Button, Callout } from "@radix-ui/themes";
+import { TextField, TextArea, Button, Callout, Text } from "@radix-ui/themes";
 import { useForm, Controller } from "react-hook-form";
 import React, { useState } from "react";
 import axios from "axios";
 import "easymde/dist/easymde.min.css";
 import { useRouter } from "next/navigation";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { createIssueSchema } from "@/app/validationSchema";
+import { z } from "zod";
 
-interface IssueForm {
-  title: string;
-  description: string;
-}
+type IssueForm = z.infer<typeof createIssueSchema>;
+// interface IssueForm {
+//   title: string;
+//   description: string;
+// }
 
 const NewIssuePage = () => {
   const router = useRouter();
-  const { register, control, handleSubmit } = useForm<IssueForm>();
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IssueForm>({
+    resolver: zodResolver(createIssueSchema),
+  });
   const [error, setError] = useState("");
 
   return (
@@ -36,10 +47,14 @@ const NewIssuePage = () => {
         })}
       >
         <TextField.Root placeholder="Title" {...register("title")} />
+        {errors.title && <Text color="red">{errors.title.message}</Text>}
         <TextArea
           placeholder="Description"
           {...register("description")}
         ></TextArea>
+        {errors.description && (
+          <Text color="red">{errors.description.message}</Text>
+        )}
 
         <Button>Submit New Issue</Button>
       </form>
