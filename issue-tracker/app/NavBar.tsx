573e90c949d44daa7700781083ac30659e388022
Varun Nambigari <nambigarivarun@gmail.com>
2024-12-15 12:58:31 +0530

    show logout in drop down menu with avatar

--- issue-tracker/app/NavBar.tsx
+++ issue-tracker/app/NavBar.tsx
@@ -5,13 +5,17 @@ import { useSession } from "next-auth/react";
 import React from "react";
 import { FaBug } from "react-icons/fa";
 import classnames from "classnames";
-import { Box, Container, Flex } from "@radix-ui/themes";
+import { Text } from "@radix-ui/themes";
+import { Avatar, Box, Container, DropdownMenu, Flex } from "@radix-ui/themes";
+// import * as Avatar from "@radix-ui/react-avatar";
+
 import AuthProvider from "./auth/provider";
 
 const NavBar = () => {
   // this hook uses browser api. So need to convert to a client component
   const currentPath = usePathname();
   const { status, data: session } = useSession();
+  console.log("session is : ", session);
   const links = [
     { label: "Dashboard", href: "/" },
     { label: "Issues", href: "/issues" },
@@ -43,7 +47,25 @@ const NavBar = () => {
           </Flex>
           <Box>
             {status === "authenticated" && (
-              <Link href="api/auth/signout">Log out</Link>
+              <DropdownMenu.Root>
+                <DropdownMenu.Trigger>
+                  <Avatar
+                    src={session.user?.image}
+                    fallback="A"
+                    size="2"
+                    radius="full"
+                    className="cursor-pointer"
+                  />
+                </DropdownMenu.Trigger>
+                <DropdownMenu.Content>
+                  <DropdownMenu.Label>
+                    <Text size="2">{session.user?.email}</Text>
+                  </DropdownMenu.Label>
+                  <DropdownMenu.Item>
+                    <Link href="api/auth/signout">Log out</Link>
+                  </DropdownMenu.Item>
+                </DropdownMenu.Content>
+              </DropdownMenu.Root>
             )}
             {status === "unauthenticated" && (
               <Link href="api/auth/signin">Log in</Link>
