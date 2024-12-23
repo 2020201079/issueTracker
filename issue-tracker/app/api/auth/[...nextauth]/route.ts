7a133e09744f302d50ece16010d199e30e193f18
Varun Nambigari <nambigarivarun@gmail.com>
2024-12-23 09:37:29 +0530

    build assignee select component

--- issue-tracker/app/api/auth/[...nextauth]/route.ts
+++ issue-tracker/app/api/auth/[...nextauth]/route.ts
@@ -1,14 +1,19 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import GitHubProvider from "next-auth/providers/github";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import prisma from "@/db/db";
 
 const handler = NextAuth({
-  adapter: PrismaAdapter(prisma),
+  //   adapter: PrismaAdapter(prisma),
   providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    // GoogleProvider({
+    //   clientId: process.env.GOOGLE_CLIENT_ID!,
+    //   clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    // }),
+    GitHubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID!,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET!,
     }),
   ],
   session: {
